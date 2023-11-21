865a9429518a6b88f39a9c9056cbeb5ce2c30756
natinhaaa <134004670+natinhaaa@users.noreply.github.com>
2023-11-21 15:50:39 -0300

    update

--- script.js
+++ script.js
@@ -1,21 +1,56 @@
 const tabela = document.querySelector('.tabela-js')
-    axios.get('http://10.109.142.139:5000/list').then((response) => {
-        getData(response.data)
-    }).catch((error) => {
-        console.log(error)
-    })
 
-function getData(data){
-    data.map((item) => {
-        tabela.innerHTML += `
-        <tr>
-            <th scope="row" class="text-center align-middle">${item.ID}</th>
-            <td class="text-center align-middle">${item.TAREFA}</td>
-            <td class="text-center align-middle">
-                <span type="button" class="material-symbols-outlined text-success">edit</span>
-                <span type="button" class="material-symbols-outlined text-danger">delete</span>
-            </td>
-        </tr>       
-        `
+// // ##################### GET #####################
+
+// axios.get('http://127.0.0.1:5000/list').then((response) => {
+//     getData(response.data)
+// }).catch((error) => {
+//     console.log('Erro na requisição GET', error)
+// });
+
+// function getData(data){
+//     data.map((item) => {
+//         tabela.innerHTML += `
+//             <tr>
+//                 <th scope="row" class="text-center align-middle">${item.ID}</th>
+//                 <td class="text-center align-middle">${item.TAREFA}</td>
+//                 <td class="text-center align-middle">
+//                     <span type="button" class="material-symbols-outlined text-success">edit</span>
+//                     <span type="button" class="material-symbols-outlined text-danger">delete</span>
+//                 </td>
+//             </tr>
+//         `
+//     })
+// }
+
+// // ##################### POST #####################
+
+const formTarefa = document.getElementById('formTarefa');
+
+formTarefa.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const tarefa = document.getElementById('exampleInputName1').value;
+
+    const Tarefa = {
+        Tarefa: tarefa
+    };
+
+    enviarDadosAPI(Tarefa);
+});
+
+function enviarDadosAPI(Tarefa) {
+    axios.post('http://127.0.0.1:5000/add', Tarefa).then(response => {
+        console.log(response.data.Tarefa);
     })
+    .catch(error => {
+        console.error('Erro na requisição POST', error);
+    });
 }
+
+
+// // ##################### PUT #####################
+
+
+
+// // ##################### DELETE #####################
