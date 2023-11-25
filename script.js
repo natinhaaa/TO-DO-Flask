6e590ea3f776ac91d4c8d6c3c0e24e0666b7c0c4
natinhaaa <natvcer2@gmail.com>
2023-11-25 20:14:23 -0300

    update

--- script.js
+++ script.js
@@ -25,36 +25,67 @@ function getData(data){
 
 // ##################### POST #####################
 
-axios.post("http://127.0.0.1:5000/add").then((response) => {
-    getData(response.data);
+// axios.post("http://127.0.0.1:5000/add").then((response) => {
+//     getData(response.data);
+// });
+
+// function AdicionarTarefa() {
+//     const NovaTarefa = document.getElementById('nova-tarefa').value;
+
+//     if (NovaTarefa === "") {
+//         alert("Insira uma nova tarefa.");
+//         return;
+//     }
+//     const UltimaTarefa = tabela.lastElementChild
+//     let ID = 1;
+
+//     if (UltimaTarefa) {
+//         ID = parseInt(UltimaTarefa.querySelector('th').textContent) + 1
+//     }
+
+//     tabela.innerHTML += `
+//         <tr>
+//             <th scope="row" class="text-center align-middle">${ID}</th>
+//             <td class="text-center align-middle">${NovaTarefa}</td>
+//             <td class="text-center align-middle">
+//                 <span type="button" class="material-symbols-outlined text-success">edit</span>
+//                 <span type="button" class="material-symbols-outlined text-danger">delete</span>
+//             </td>
+//         </tr>          
+//     `
+
+//     document.getElementById('nova-tarefa').value = ""
+// }
+
+const ModalAdicionar = document.getElementById('formAdicionar');
+
+ModalAdicionar.addEventListener('submit', (event) => {
+    // previne que qualquer outra ação interrompa o evento, o evento só ocorrerá se a ação do usuário for 'submit'
+    event.preventDefault();
+
+    // pegará o valor dado pelo usuário no campo de adicionar nova tarefa
+    const TarefaInserida = document.getElementById('nova-tarefa').value;
+    if (NovaTarefa === "") {
+            alert("Insira uma nova tarefa.");
+            return;
+    }
+    else {
+        const TarefaNova = {
+            Tarefa: TarefaInserida
+        };
+        SalvarCSV(TarefaNova);
+    }
+
 });
 
-function AdicionarTarefa() {
-    const NovaTarefa = document.getElementById('nova-tarefa').value;
-
-    if (NovaTarefa === "") {
-        alert("Insira uma nova tarefa.");
-        return;
-    }
-    const UltimaTarefa = tabela.lastElementChild
-    let ID = 1;
-
-    if (UltimaTarefa) {
-        ID = parseInt(UltimaTarefa.querySelector('th').textContent) + 1
-    }
-
-    tabela.innerHTML += `
-        <tr>
-            <th scope="row" class="text-center align-middle">${ID}</th>
-            <td class="text-center align-middle">${NovaTarefa}</td>
-            <td class="text-center align-middle">
-                <span type="button" class="material-symbols-outlined text-success">edit</span>
-                <span type="button" class="material-symbols-outlined text-danger">delete</span>
-            </td>
-        </tr>          
-    `
-
-    document.getElementById('nova-tarefa').value = ""
+function SalvarCSV(TarefaNova) {
+    axios.post('http://127.0.0.1:5000/add', TarefaNova)
+        .then(response => {
+            console.log(response.data.TarefaNova);
+        })
+        .catch(error => {
+            console.error('Erro na requisição POST', error);
+        });
 }
 
 // ##################### PUT #####################
